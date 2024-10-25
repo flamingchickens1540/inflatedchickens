da179a50b28ff326d2b2da7aa2fc56330a8410cb
NateLydem <146988904+belowAverageCoder5188@users.noreply.github.com>
2024-10-24 17:09:53 -0700

    fix: made readable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
 	import { actionResult, actionType, type ActionData } from './action';
 	import Timeline from './Timeline.svelte';
-	function thingy() {latestActions.push({type: actionType.EjectTote, result: actionResult.success})}
 
-	let latestActions: ActionData[] = $state([{type: actionType.EjectTote, result: actionResult.fail}]);
-	latestActions.push({type: actionType.IntakeTote, result: actionResult.success})
+	//testing
+	function thingy() {
+		latestActions.push({ type: actionType.EjectTote, result: actionResult.success });
+	}
+
+	let latestActions: ActionData[] = $state([
+		{ type: actionType.EjectTote, result: actionResult.fail }
+	]);
+	latestActions.push({ type: actionType.IntakeTote, result: actionResult.success });
 </script>
 
 <Timeline actions={latestActions} />
-<button on:click={thingy}>UwU</button>
+
+<!--testing-->
+<button onclick={thingy}>UwU</button>
 
 <style>
 	:global(body) {
