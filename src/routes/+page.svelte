87db4237f501aeca993a96b4e3965dd4cce159c9
awwpotato <awwpotato@voidq.com>
2024-10-25 17:35:11 -0700

    fix: add homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,3 @@
-<script lang="ts">
-	import { actionResult, actionType, type ActionData } from '$lib/types';
-	import Timeline from '$lib/components/Timeline.svelte';
+<h1>hiiii :3</h1>
 
-	function addAction() {
-		//everything here is for testing, as there is no system for this yet
-		latestActions.push({ type: actionType.ScoreAnotherRobotsTote, result: actionResult.success });
-		latestActions.push({ type: actionType.EjectTote, result: actionResult.fail });
-		latestActions.push({ type: actionType.IntakeTote, result: actionResult.success });
-	}
-
-	let latestActions: ActionData[] = $state([]);
-</script>
-
-<Timeline bind:actions={latestActions} />
-
-<!--to be changed in the future-->
-<button onclick={addAction}>Add Action</button>
-
-<style>
-	:global(body) {
-		background-color: rgb(40, 40, 50);
-	}
-</style>
+<a href="/scout">scount</a>
