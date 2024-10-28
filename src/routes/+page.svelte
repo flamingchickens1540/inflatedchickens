dbd1684cd2ebe55ca928ee6c1d0863782ea7957b
awwpotato <awwpotato@voidq.com>
2024-10-28 16:38:23 -0700

    update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<h1 class="text-center">hiiii :3</h1>
-
-<a class="text-center" href="/scout">scount</a>
+<div class="flex flex-col items-center justify-evenly h-dvh text-text_white">
+    <h1 class="text-center text-5xl font-bold">
+        hiiii :3
+    </h1>
+    <a class="text-center text-2xl rounded bg-btn_grey px-4 py-2 font-bold" href="/scout">
+        scout
+    </a>
+</div>
