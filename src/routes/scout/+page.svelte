9e5ea52b81d4b8d0dbf7614ab6341ed8c4f314f1
Nate L. <146988904+NateLydem@users.noreply.github.com>
2024-11-05 00:04:56 +0000

    moved actions to top of timeline (#2)

--- src/routes/scout/+page.svelte
+++ src/routes/scout/+page.svelte
@@ -4,19 +4,17 @@
 
 	function addAction() {
 		//everything here is for testing, as there is no system for this yet
-		latestActions.push({ type: actionType.ScoreAnotherRobotsTote, result: actionResult.success });
-		latestActions.push({ type: actionType.EjectTote, result: actionResult.fail });
-		latestActions.push({ type: actionType.IntakeTote, result: actionResult.success });
+		actions.push({ type: actionType.ScoreAnotherRobotsTote, result: actionResult.success });
+		actions.push({ type: actionType.EjectTote, result: actionResult.fail });
+		actions.push({ type: actionType.IntakeTote, result: actionResult.success });
 	}
 
-	let latestActions: ActionData[] = $state([]);
+	let actions: ActionData[] = $state([]);
 </script>
 
 <main class="flex flex-col items-center gap-2 p-2 justify-center h-screen">
-	<Timeline bind:actions={latestActions} />
+	<Timeline bind:actions={actions} />
 
 	<!--to be changed in the future-->
-	<button class="bg-btn_grey w-80 p-1 rounded border-2 border-outline_gray" onclick={addAction}
-		>Add Action</button
-	>
+	<button class="bg-btn_grey w-80 p-1 rounded border-2 border-outline_gray" onclick={addAction}>Add Action</button>
 </main>
