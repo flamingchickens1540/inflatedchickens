f5152900e77cd0c3997b75ba74c0368d2625c270
awwpotato <awwpotato@voidq.com>
2024-11-05 19:59:16 -0800

    theme fixes

--- src/lib/components/SuccessFail.svelte
+++ src/lib/components/SuccessFail.svelte
@@ -3,9 +3,18 @@
 </script>
 
 <div
-	class="bg-zinc-800 text-lg fixed h-screen w-screen grid grid-cols-2 grid-rows-3 auto-cols-max gap-4 p-4"
+	class="bg-zinc-800 text-zinc-50 text-xl font-extrabold fixed inset-0 grid grid-cols-2 grid-rows-3 gap-4 p-4"
 >
-	<button class="rounded-lg row-span-2 bg-green-500" onclick={() => complete(true)}>Success</button>
-	<button class="rounded-lg row-span-2 bg-red-500" onclick={() => complete(false)}>Fail</button>
-	<button class="rounded-lg col-span-2 bg-zinc-500" onclick={() => cancel()}>Cancel</button>
+	<button
+		class="rounded-lg row-span-2 shadow-lg shadow-green-600/50 bg-green-600"
+		onclick={() => complete(true)}>Success</button
+	>
+	<button
+		class="rounded-lg row-span-2 shadow-lg shadow-red-600/50 bg-red-600"
+		onclick={() => complete(false)}>Fail</button
+	>
+	<button
+		class="rounded-lg col-span-2 shadow-lg shadow-zinc-600/50 bg-zinc-600"
+		onclick={() => cancel()}>Cancel</button
+	>
 </div>
--- src/routes/scout/+page.svelte
+++ src/routes/scout/+page.svelte
@@ -19,20 +19,25 @@
 	};
 
 	let { match_key, team_key }: { match_key: string; team_key: string } = $props();
+	team_key = '1540';
 	let match: TeamMatch = $state({
 		team_key,
 		match_key,
 		auto_actions: [],
 		tele_actions: []
 	});
+
+	$effect(() => console.log(actionState));
 </script>
 
-<main class="text-text_white">
+<main class="text-zinc-50 flex flex-col p-2 h-svh">
 	{#if actionState != 'None'}
 		<SuccessFail {complete} cancel={() => (actionState = 'None')} />
 	{/if}
-	{#if actionState == 'None'}
-		<button>oagjoigoadg</button>
-	{/if}
-	<button class="bg-btn_grey p-2 rounded" onclick={() => score_low('Balloon')}> Score </button>
+	<span class="text-center font-bold pb-2">team {team_key}</span>
+	<div class="grid gap-2 grid-cols-2 flex-grow">
+		<button class="bg-zinc-500 p-2 rounded" onclick={() => score_low('Balloon')}> Score </button>
+		<button class="bg-zinc-500 p-2 rounded" onclick={() => (actionState = 'Intake')}>Intake</button>
+		<button class="bg-zinc-500 p-2 rounded col-span-2"> Timeline </button>
+	</div>
 </main>
