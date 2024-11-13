d97b11175e972f1883f81577c8007a607381a2a7
Brandon Harad <haradb@catlin.edu>
2024-11-13 15:50:15 -0800

    feat: pulled from main

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,22 +1,3 @@
-export enum actionType {
-	IntakeTote = 'Intake Tote',
-	EjectTote = 'Eject Tote',
-	IntakeBalloon = 'Intake Balloon',
-	ScoreLow = 'Score Low',
-	ScoreExternalTote = 'Score External Tote',
-	ScoreInternalTote = 'Score Internal Tote',
-	ScoreAnotherRobotsTote = 'Score Another Robots Tote'
-}
-
-export enum actionResult {
-	success = 'success',
-	fail = 'fail'
-}
-
-export type ActionData = {
-	type: actionType;
-	result: actionResult;
-};
 export type TeamMatch = {
 	team_key: string;
 	match_key: string;
