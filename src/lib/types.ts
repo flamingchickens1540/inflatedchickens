938b296cdc8f0fd58a932dd4d1fcd951316f0bd0
Azalea Colburn <62953415+azaleacolburn@users.noreply.github.com>
2024-11-13 19:45:26 -0800

    Action Input Grid (#6)
    * feat: type model for scouting system
    
    * chore: update svelte
    
    * feat: add none
    
    * refactor: type state model
    
    * feat: state transforms
    
    * add successfail screen
    
    * theme fixes
    
    * fix: package.json
    
    * fix: formating
    
    * fix: dependencies
    
    * format thingy
    
    * refactor: update types to match new schema
    
    * fix: id after clarifying with Brandon
    
    * update dep
    
    * style: format files
    
    * refactor: action input state machine into component
    
    * feat: make cancel buttons go back one level
    
    * fix: actions display on timeline
    
    * feat: piece specific error prevention
    
    * feat: ejectables!
    
    * fix: name in pkg.json + update dep
    
    * style: format files
    
    ---------
    
    Co-authored-by: awwpotato <awwpotato@voidq.com>

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,19 +1,95 @@
-export enum actionType {
-	IntakeTote = 'Intake Tote',
-	EjectTote = 'Eject Tote',
-	IntakeBalloon = 'Intake Balloon',
-	ScoreLow = 'Score Low',
-	ScoreExternalTote = 'Score External Tote',
-	ScoreInternalTote = 'Score Internal Tote',
-	ScoreAnotherRobotsTote = 'Score Another Robots Tote'
-}
-
-export enum actionResult {
-	success = 'success',
-	fail = 'fail'
-}
-
-export type ActionData = {
-	type: actionType;
-	result: actionResult;
+// For DB
+export type Match = {
+	match_key: string;
+	event_key: string;
 };
+
+export type User = {
+	id: string;
+	name: string;
+	is_admin: boolean;
+	slack_token: string;
+};
+
+/// Counts
+export type AutoActionsTM = ActionsTM & {
+	bunny_intake_success: number;
+	bunny_intake_failure: number;
+	bunny_tote_success: number;
+	bunny_tote_failure: number;
+	bunny_low_success: number;
+	bunny_low_failure: number;
+	actions: AutoActionData[];
+};
+
+export type TeleActionsTM = ActionsTM & {
+	actions: TeleActionData[];
+};
+
+export type ActionsTM = {
+	id: number;
+	tote_intake_success: number;
+	tote_intake_failure: number;
+	tote_eject_success: number;
+	tote_eject_failure: number;
+	balloon_intake_success: number;
+	bollon_intake_failure: number;
+	score_low_success: number;
+	score_low_failure: number;
+	score_internal_success: number;
+	score_internal_failure: number;
+	score_external_success: number;
+	score_external_failure: number;
+	score_other_robot_success: number;
+	score_other_robot_failure: number;
+};
+
+export type TeamMatch = {
+	id: number;
+	scout_id: string;
+	match_key: string;
+	team_key: string;
+	skill: number;
+	notes: string;
+	broke: boolean;
+	died: boolean;
+	auto_actions: AutoActionData[];
+	tele_actions: TeleActionData[];
+};
+
+export type AutoActionData = {
+	action: AutoAction;
+	success: boolean;
+};
+
+export type TeleActionData = {
+	action: TeleAction;
+	success: boolean;
+};
+
+// Action Types
+// Naming Convention: action_type + game_piece + where
+export type TeleAction =
+	| 'IntakeTote'
+	| 'IntakeBalloon'
+	| 'IntakeBalloonCoral'
+	| 'ScoreBalloonInternalTote' // Held by scorer
+	| 'ScoreBalloonExternalTote' // Held by alliance member
+	| 'ScoreBalloonUncontrolledTote'
+	| 'ScoreBalloonLow'
+	| 'EjectBalloon'
+	| 'EjectBunny' // Could happen in Tele; we could instead move this to BunnyAction and reset held_bunnies to 0 after Auto
+	| 'EjectTote';
+
+export type BunnyAction =
+	| 'IntakeBunny'
+	| 'ScoreBunnyInternalTote'
+	| 'ScoreBunnyExternalTote'
+	| 'ScoreBunnyUncontrolledTote'
+	| 'ScoreBunnyLow';
+export type AutoAction = TeleAction | BunnyAction;
+
+// For state machine
+export type ItemInputState = 'Intake' | 'Score' | 'Eject' | 'None';
+export type TeleInputState = TeleAction | ItemInputState;
+export type AutoInputState = TeleInputState | BunnyAction;
--- src/routes/scout/+page.svelte
+++ src/routes/scout/+page.svelte
@@ -1,33 +1,49 @@
 <script lang="ts">
-	import { actionResult, actionType, type ActionData } from '$lib/types';
+	import { type TeamMatch, type AutoActionData } from '$lib/types';
 	import Timeline from '$lib/components/Timeline.svelte';
+	import ActionInputs from './ActionInputs.svelte';
 
-	function addAction() {
-		//everything here is for testing, as there is no system for this yet
-		actions.push({ type: actionType.ScoreAnotherRobotsTote, result: actionResult.success });
-		actions.push({ type: actionType.EjectTote, result: actionResult.fail });
-		actions.push({ type: actionType.IntakeTote, result: actionResult.success });
-	}
+	const {
+		match_key,
+		team_key = '1540',
+		scout_id
+	}: { match_key: string; team_key: string; scout_id: string } = $props();
 
+	let actions: AutoActionData[] = $state([]);
 	let timelineExtended = $state(false);
-	function toggleTimeline() {
-		timelineExtended = !timelineExtended;
-	}
 
-	let latestActions: ActionData[] = $state([]);
+	const match: TeamMatch = $state({
+		id: 0,
+		scout_id,
+		team_key,
+		match_key,
+		skill: 3,
+		notes: '',
+		broke: false,
+		died: false,
+		auto_actions: [],
+		tele_actions: []
+	});
 </script>
 
-<main class="flex flex-col items-center gap-2 p-2 justify-center h-screen">
-	<Timeline bind:actions={latestActions} bind:displaying={timelineExtended} />
-	<div class="flex flex-col grow justify-end">
-		<!--this button to be changed in the future (deleted)-->
+<div class="grid grid-row-10 text-zinc-50 p-2 h-svh place-items-center">
+	<h1 class="row-span-1 text-center font-bold pb-2 h-5">Team {team_key}</h1>
+	{#if timelineExtended}
+		<div class="row-span-8">
+			<Timeline bind:actions />
+		</div>
 		<button
-			class="bg-btn_grey w-80 p-1 rounded border-2 border-outline_gray static"
-			onclick={addAction}>Add Action</button
+			class="row-span-1 bg-btn_grey h-10 w-80 p-1 rounded border-2 border-outline_gray static"
+			onclick={() => (timelineExtended = false)}>Hide Timeline</button
 		>
+	{:else}
+		<div class="row-span-8">
+			<ActionInputs bind:actions />
+		</div>
+
 		<button
-			class="bg-btn_grey w-80 p-1 rounded border-2 border-outline_gray static"
-			onclick={toggleTimeline}>Show Timeline</button
+			class="row-span-1 bg-btn_grey h-10 w-80 p-1 rounded border-2 border-outline_gray static"
+			onclick={() => (timelineExtended = true)}>Show Timeline</button
 		>
-	</div>
-</main>
+	{/if}
+</div>
