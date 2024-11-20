7e0c5fedf7537d1848d95cf85fdee7717e0ec320
azaleacolburn <azaleacolburn@users.noreply.github.com>
2024-11-20 00:35:29 +0000

    style: format files

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -21,7 +21,7 @@
 	};
 </script>
 
-<div class="text-white grid m-4 gap-4 place-items-center">
+<div class="m-4 grid place-items-center gap-4 text-white">
 	<input
 		bind:value={match_key}
 		placeholder="Match Key"
@@ -29,17 +29,17 @@
 		class="rounded p-2 text-black"
 	/>
 
-	<div class="grid grid-rows-4 grid-flow-col gap-2">
-		<div class="grid grid-cols-2 grid-rows-1 text-xl gap-4">
+	<div class="grid grid-flow-col grid-rows-4 gap-2">
+		<div class="grid grid-cols-2 grid-rows-1 gap-4 text-xl">
 			<h1>Red</h1>
 			<h1>Blue</h1>
 		</div>
-		<div class="grid grid-cols-2 grid-rows-3 row-span-3 gap-4 text-black grid-flow-col">
+		<div class="row-span-3 grid grid-flow-col grid-cols-2 grid-rows-3 gap-4 text-black">
 			{#each teams as _, i}
 				<input bind:value={teams[i]} type="text" class="rounded p-1" />
 			{/each}
 		</div>
 	</div>
 
-	<button onclick={queue_match} class="p-2 rounded border">Queue Match</button>
+	<button onclick={queue_match} class="rounded border p-2">Queue Match</button>
 </div>
