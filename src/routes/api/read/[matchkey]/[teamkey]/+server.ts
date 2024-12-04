74c4124ea8d46e8a7c3cec4347294a34f125faa9
Brandon-Harad <Brandon-Harad@users.noreply.github.com>
2024-12-04 23:53:00 +0000

    style: format files

--- src/routes/api/read/[matchkey]/[teamkey]/+server.ts
+++ src/routes/api/read/[matchkey]/[teamkey]/+server.ts
@@ -2,6 +2,6 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { select } from '$lib/server-assets/database';
 
-export const GET: RequestHandler = async ( {params} : any ) => {
+export const GET: RequestHandler = async ({ params }: any) => {
 	return json(await select(params.matchkey, params.teamkey));
 };
