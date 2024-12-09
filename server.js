52bd0e9378b5393b3aa57d048c3e5e496554af8d
Azalea Colburn <azalea.colburn@autodesk.com>
2024-12-08 22:17:59 -0800

    wip: server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var http_1 = require('http');
-var express_1 = require('express');
-var ws_1 = require('./ws');
-var handler_js_1 = require('./build/handler.js');
-var dotenv = require('dotenv');
-dotenv.config();
-var app = (0, express_1.default)();
-var server = http_1.default.createServer(app);
-// Inject SocketIO
-(0, ws_1.default)(server);
-// SvelteKit handlers
-app.use(handler_js_1.handler);
-server.listen(process.env.PUBLIC_WS_ENDPOINT, function () {
-	console.log('Running on http://localhost:' + PUBLIC_WS_ENDPOINT);
+import { express } from 'express';
+import { createServer } from 'http';
+import { handler } from './build/handler.js';
+import injectSocketIO from './ws';
+
+const app = express();
+const server = createServer(app);
+const port = 3022;
+
+const io = injectSocketIO(server);
+
+app.use(handler);
+
+app.listen(port, () => {
+	console.log(`WS Server Listening On Port ${port}`);
 });
+
+export const getServer = () => io;
