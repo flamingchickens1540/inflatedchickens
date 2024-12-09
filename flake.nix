bf770c6f676e12ed55a752b0f4b833b62071f927
awwpotato <153149335+awwpotato@users.noreply.github.com>
2024-12-08 21:23:07 -0800

    add nix flake dev shell (#18)
    * init: nix dev flake
    
    * style: format files
    
    * fix editor config for nix files
    
    * don't format lock file
    
    ---------
    
    Co-authored-by: awwpotato <awwpotato@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    systems,
+  }:
+    flake-utils.lib.eachSystem (import systems)
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bun
+        ];
+      };
+    });
+}
