cd8e4ca12ee5a14c543b2fcd6d68ac36e0083134
awwpotato <awwpotato@users.noreply.github.com>
2024-12-10 17:32:09 +0000

    style: format files

--- src/routes/scout/[team_data]/Postmatch.svelte
+++ src/routes/scout/[team_data]/Postmatch.svelte
@@ -17,8 +17,10 @@
 <div class="flex w-full flex-col gap-2 accent-yolk_yellow">
 	<h1 class="text-center font-heading text-2xl">Post Match</h1>
 	<div class="rounded-lg bg-gunmetal p-1">
-		<h2 class="font-heading text-2xl mb-1">Driver Skill</h2>
-		<div class="flex w-full text-lg flex-wrap items-center justify-center rounded-lg bg-eerie_black">
+		<h2 class="mb-1 font-heading text-2xl">Driver Skill</h2>
+		<div
+			class="flex w-full flex-wrap items-center justify-center rounded-lg bg-eerie_black text-lg"
+		>
 			<div class="flex flex-col p-1 text-center">
 				Quickness: {speed}
 				<input class="w-36" type="range" min="0" max="5" step="1" bind:value={speed} />
@@ -30,7 +32,7 @@
 		</div>
 	</div>
 	<div class="rounded-lg bg-gunmetal p-1">
-		<h2 class="font-heading text-2xl mb-1">Robot</h2>
+		<h2 class="mb-1 font-heading text-2xl">Robot</h2>
 		<div class="flex flex-col gap-2 rounded bg-eerie_black p-2">
 			<label
 				><input type="checkbox" name="robotbroke" bind:checked={broke} /><span class="px-1"
@@ -45,9 +47,9 @@
 		</div>
 	</div>
 	<label class="w-full rounded-lg bg-gunmetal p-1"
-		><h3 class="font-heading text-2xl mb-1">Notes</h3>
+		><h3 class="mb-1 font-heading text-2xl">Notes</h3>
 		<textarea
-			class="w-full rounded bg-eerie_black p-1 min-h-36"
+			class="min-h-36 w-full rounded bg-eerie_black p-1"
 			bind:value={notes}
 			placeholder="Please add notes"
 		></textarea>
